f5b1731a69a6ef7824b884c098abd7f3ad381e06
Benjababe <bengohzy@gmail.com>
2023-01-16 18:01:01 +0800

    p2 example update

--- example/src/App.js
+++ example/src/App.js
@@ -1,24 +1,14 @@
-import { useState } from 'react';
+import Note from './components/Note';
 
-const Display = props => <div>{props.value}</div>
+const App = ({ notes }) => {
+    return (
+        <div>
+            <h1>Notes</h1>
+            <ul>
+                {notes.map(note => <Note key={note.id} note={note} />)}
+            </ul>
+        </div>
+    );
+};
 
-const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>
-    {text}
-  </button>
-)
-
-const App = () => {
-  const [value, setToValue] = useState(0);
-
-  return (
-    <div>
-      <Display value={value} />
-      <Button handleClick={() => setToValue(1000)} text="thousand" />
-      <Button handleClick={() => setToValue(0)} text="zero" />
-      <Button handleClick={() => setToValue(value + 1)} text="increment" />
-    </div>
-  )
-}
-
-export default App;
+export default App
